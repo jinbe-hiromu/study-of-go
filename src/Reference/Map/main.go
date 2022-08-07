7cf3126fabe26ea47af974aa29b7d5be9dd0ebb4
hiromu.matsubara <matsubara0501jp@gmail.com>
2022-08-07 13:24:43 +0900

    Study for map

--- src/Reference/Map/main.go
+++ src/Reference/Map/main.go
@@ -5,60 +5,48 @@ import (
 )
 
 func main() {
-	var slice []int
-	fmt.Println(slice)
+	// 明示的
+	var map1 = map[string]int{"A": 100, "B": 200}
+	fmt.Println(map1)
 
-	var slice2 []int = []int{100, 200}
-	fmt.Println(slice2)
+	// 暗黙的
+	map2 := map[string]int{"A": 100, "B": 200}
+	fmt.Println(map2)
 
-	slice2[0] = 1000
-	fmt.Println(slice2)
+	// makeでmapを生成
+	map3 := make(map[string]int)
 
-	slice3 := []string{"A", "B"}
-	fmt.Println((slice3))
+	// 値の追加
+	map3["A"] = 100
+	map3["B"] = 200
+	fmt.Println(map3)
 
-	slice4 := make([]int, 5)
-	fmt.Println(slice4) // 0 0 0 0 0
+	// 値の変更
+	map3["A"] = 1000
+	fmt.Println(map3)
 
-	slice5 := []int{0, 1, 2, 3, 4}
-	// 要素1~2の取り出し(先頭が以上，末尾が未満になっている！)
-	fmt.Println(slice5[1:3]) // 1 2
-	// 要素3より小さいものの取り出し
-	fmt.Println(slice5[:3]) // 0 1 2
-	// 要素3以上の取り出し
-	fmt.Println(slice5[3:]) // 3 4
-	// 最後の要素の取り出し
-	fmt.Println(slice5[len(slice5)-1]) // 4
+	// 値の削除
+	delete(map3, "A")
+	fmt.Println(map3)
 
-	// append
-	sl := []int{100, 200}
-	sl = append(sl, 300)
-	fmt.Println(sl) // 100 200 300
+	// キーがない場合の値
+	fmt.Println(map3["C"]) // 空文字　mapで指定したキーが存在しない場合，空文字が出力される
 
-	// append(複数)
-	sl = append(sl, 400, 500)
-	fmt.Println(sl) // 100 200 300 400 500
-
-	// スライスの代入
-	sl2 := sl
-	sl2[0] = 1000    // 参照型でコピーされるので代入されると元の配列も影響を受ける
-	fmt.Println(sl)  // 1000 200 300 400 500
-	fmt.Println(sl2) // 1000 200 300 400 500
-
-	// copy
-	copyed := make([]int, 5) // 先にコピー先をmakeで作成
-	copyedNum := copy(copyed, sl)
-	fmt.Println("コピーされた数", copyedNum)
-
-	copyed[0] = 10000
-	fmt.Println("src", sl)        // 1000 200 300 400 500		←ソースには影響していない．
-	fmt.Println("copyed", copyed) // 10000 200 300 400 500
-
-	// froeach
-	for index, value := range sl {
-		fmt.Println("index=", index, "value=", value)
+	// 値取得に失敗した場合のエラーハンドリング
+	value, ret := map3["C"]
+	if !ret {
+		fmt.Println("error") // キーが存在しないので，こちらが出力
+	} else {
+		fmt.Println(value)
 	}
 
-	// Sum関数の呼び出し *スライスを引数にする場合「...」を変数の後に置く
-	fmt.Println(Sum(sl...))
+	// foreach
+	map4 := map[string]int{
+		"Apple":  100,
+		"Banana": 200,
+	}
+
+	for k, v := range map4 {
+		fmt.Println(k, v)
+	}
 }
